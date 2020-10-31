ee40dcd38cdafbf5a31663172476f1bd1943f4bd
lsr <imlimr1232@gmail.com>
2020-10-31 11:23:00 +0900

    Styling nav

--- screens/Movie.js
+++ screens/Movie.js
@@ -3,8 +3,8 @@ import { View, Text, Button } from "react-native";
 
 const Movie = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <Text style={{ color: "white" }}>Home</Text>
       <Button
         onPress={() => navigation.navigate("Detail")}
         title="Go to Detail"

fdb2b0f7fda6abc8e37a601d984206a7cafad4e8
lsr <imlimr1232@gmail.com>
2020-10-31 11:15:14 +0900

    Update Header

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movie from "../screens/Movie";
 import Tv from "../screens/Tv";
@@ -7,11 +7,21 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-  <Tabs.Navigator>
-    <Tabs.Screen name="Movie" component={Movie} />
-    <Tabs.Screen name="Tv" component={Tv} />
-    <Tabs.Screen name="Search" component={Search} />
-    <Tabs.Screen name="Favs" component={Favs} />
-  </Tabs.Navigator>
-);
+const getHeaderName = (route) =>
+  route?.state?.routeNames[route.state.index] || "Movie";
+
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: getHeaderName(route),
+    });
+  }, [route]);
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Movie" component={Movie} />
+      <Tabs.Screen name="Tv" component={Tv} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favs" component={Favs} />
+    </Tabs.Navigator>
+  );
+};

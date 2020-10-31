3075f3b566070c920cca4e97db5e71dfd065ff9b
lsr <imlimr1232@gmail.com>
2020-10-31 12:00:59 +0900

    add axios & 네트워킹

---
+++ api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const TMDB_KEY = "d59ec51c3d61c3f26f6642570acbfcdf";
+
+const makeRequest = (path, params) =>
+  axios.get(`https://api.themoviedb.org/3${path}`, {
+    params: {
+      ...params,
+      api_key: TMDB_KEY,
+    },
+  });
+
+const getAnything = async (path, params = {}) => {
+  try {
+    const {
+      data: { results },
+      data,
+    } = await makeRequest(path, params);
+    return [results || data, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+export const movieApi = {
+  nowPlaying: () => getAnything("/movie/now_playing"),
+  popular: () => getAnything("/movie/popular"),
+  upcoming: () => getAnything("/movie/upcoming"),
+  detail: (id) => getAnything(`/movie/${id}`, { append_to_response: "videos" }),
+  search: (query) => getAnything("/search/movie", { query }),
+  similar: (id) => getAnything(`/movie/${id}/similar`),
+};
+
+export const tvApi = {
+  today: () => getAnything("/tv/airing_today"),
+  popular: () => getAnything("/tv/popular"),
+  topRated: () => getAnything("/tv/top_rated"),
+  thisWeek: () => getAnything("/tv/on_the_air"),
+  detail: (id) => getAnything(`/tv/${id}`, { append_to_response: "videos" }),
+  search: (query) => getAnything("/tv/tv", { query }),
+  similar: (id) => getAnything(`/tv/${id}/similar`),
+};
--- screens/Movie.js
+++ screens/Movie.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import { movieApi } from "../api";
 
 const Movie = ({ navigation }) => {
+  const [movies, setMovies] = useState({
+    results: [],
+    error: null,
+  });
+
+  const getData = async () => {
+    const [results, error] = await movieApi.nowPlaying();
+    setMovies({
+      results,
+      error,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Text style={{ color: "white" }}>Home</Text>
+      <Text style={{ color: "white" }}>{movies.results.length}</Text>
       <Button
         onPress={() => navigation.navigate("Detail")}
         title="Go to Detail"

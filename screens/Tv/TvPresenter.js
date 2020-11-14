ca42b82fa53b66a2c67ac3f15c250772fb78985a
lsr <imlimr1232@gmail.com>
2020-11-14 10:22:06 +0900

    Finishing the TV

--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -1,6 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { ActivityIndicator, ScrollView } from "react-native";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import Title from "../../components/Title";
+import Vertical from "../../components/Vertical";
+import Horizontal from "../../components/Horizontal";
+
+const Container = styled.View``;
+
+const PopularContainer = styled.View`
+  margin-top: 20px;
+`;
 
 const TvPresenter = ({
   today,
@@ -11,9 +21,87 @@ const TvPresenter = ({
   error,
 }) => {
   return (
-    <View>
-      <Text>{today.length}</Text>
-    </View>
+    <ScrollView
+      style={{ backgroundColor: "black" }}
+      contentContainerStyle={{
+        flex: loading ? 1 : "auto",
+        justifyContent: loading ? "center" : "flex",
+      }}
+    >
+      {loading ? (
+        <ActivityIndicator color="white" size="small" />
+      ) : (
+        <>
+          <Container>
+            <Title title={"Today Shows"} />
+            <ScrollView
+              style={{ marginTop: 15, marginBottom: 20 }}
+              contentContainerStyle={{ paddingLeft: 15 }}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              {today.map((item) => (
+                <Vertical
+                  key={item.id}
+                  id={item.id}
+                  poster={item.poster_path}
+                  title={item.original_name}
+                  votes={item.vote_average}
+                />
+              ))}
+            </ScrollView>
+            <Title title={"This Week"} />
+            <ScrollView
+              style={{ marginTop: 15, marginBottom: 20 }}
+              contentContainerStyle={{ paddingLeft: 15 }}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              {thisWeek.map((item) => (
+                <Vertical
+                  key={item.id}
+                  id={item.id}
+                  poster={item.poster_path}
+                  title={item.original_name}
+                  votes={item.vote_average}
+                />
+              ))}
+            </ScrollView>
+            <Title title={"TopRated Shows"} />
+            <ScrollView
+              style={{ marginTop: 15, marginBottom: 20 }}
+              contentContainerStyle={{ paddingLeft: 15 }}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              {topRated.map((item) => (
+                <Vertical
+                  key={item.id}
+                  id={item.id}
+                  poster={item.poster_path}
+                  title={item.original_name}
+                  votes={item.vote_average}
+                />
+              ))}
+            </ScrollView>
+            <Title title={"Popular Shows"} />
+            <PopularContainer>
+              {popular.map((item) => (
+                <Horizontal
+                  key={item.id}
+                  id={item.id}
+                  title={item.original_name}
+                  votes={item.vote_average}
+                  poster={item.poster_path}
+                  overview={item.overview}
+                  releaseDate={item.first_air_date}
+                />
+              ))}
+            </PopularContainer>
+          </Container>
+        </>
+      )}
+    </ScrollView>
   );
 };
 
